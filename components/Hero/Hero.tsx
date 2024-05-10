e0cfd8f567e0ffcd712b90cdec7a860003f57614
Demo Robot <demo11website@gmail.com>
2024-05-10 16:36:35 +0500

    Revert "chutiya animation"
    This reverts commit 9c7c32d6e115204edc97bd9bd274d5b04f8f9fa2.

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,7 +1,6 @@
 import { PathFive } from "../Svg/Svg";
-import RedoAnimText from "./RedoAnimText";
 import s from "./hero.module.scss";
-// import Typewriter, { TypewriterClass } from "typewriter-effect";
+import Typewriter, { TypewriterClass } from "typewriter-effect";
 
 const Hero = () => {
   return (
@@ -9,13 +8,7 @@ const Hero = () => {
       <div className={s.pathFive}>
         <PathFive />
       </div>
-      <h5 className={s.text}>
-        <RedoAnimText
-          delay={0}
-          text="Philippe Layani is a Berlin- and Vienna-based digital UX designer who   blends creativity, technology, and strategy to develop exceptional,  tailor-made interactive experiences that connect brands with theiraudiences."
-        />
-      </h5>
-      {/* <Typewriter
+      <Typewriter
         component={"h3"}
         options={{
           strings:
@@ -23,7 +16,7 @@ const Hero = () => {
           autoStart: true,
           delay: 30,
         }}
-      /> */}
+      />
     </section>
   );
 };
