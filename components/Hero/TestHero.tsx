090c6a75954cc8abff1fec35d08a4be20907581c
Demo Robot <demo11website@gmail.com>
2024-07-20 17:16:10 +0500

    add responsive lottie

--- components/Hero/TestHero.tsx
+++ components/Hero/TestHero.tsx
@@ -1,22 +1,17 @@
 import s from "./hero.module.scss";
 import { PathFive } from "../Svg/Svg";
 import ArrowDown from "./lottie/arrow-down.json";
+import DataM from "./lottie/data-m.json";
+import DataT from "./lottie/data-t.json";
 import Data from "./lottie/data.json";
-import { useLottie } from "lottie-react";
 import { useScrollTo } from "react-use-window-scroll";
+import useWindowSize from "@/hooks/useWindowSize";
+import Lottie from "react-lottie-player";
 
 const TestHero = () => {
-  const { View: ArrowView } = useLottie({
-    animationData: ArrowDown,
-    loop: true,
-  });
-
-  const { View } = useLottie({
-    animationData: Data,
-    loop: true,
-  });
-
   const scrollTo = useScrollTo();
+
+  const size = useWindowSize();
 
   return (
     <section id="hero" className={s.main}>
@@ -24,12 +19,22 @@ const TestHero = () => {
         onClick={() => scrollTo({ top: innerHeight, behavior: "smooth" })}
         className={s.arrow}
       >
-        {ArrowView}
+        <Lottie loop animationData={ArrowDown} play />
       </div>
       <div className={s.pathFive}>
         <PathFive />
       </div>
-      <div className={s.heading}>{View}</div>
+      <div className={s.heading}>
+        {size.width! > 640 ? (
+          size.width! > 840 ? (
+            <Lottie loop animationData={Data} play />
+          ) : (
+            <Lottie loop animationData={DataT} play />
+          )
+        ) : (
+          <Lottie loop animationData={DataM} play />
+        )}
+      </div>
     </section>
   );
 };
